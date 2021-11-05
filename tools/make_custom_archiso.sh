f9e7d545c5a59770690df9b8edfe6c5a371734ea
leanhtai01 <leanhtai01@gmail.com>
2021-11-05 11:49:40 +0700

    get new change from remote repos

--- tools/make_custom_archiso.sh
+++ tools/make_custom_archiso.sh
@@ -6,9 +6,17 @@ current_dir=$(dirname $0)
 
 cp -r /run/media/leanhtai01/data_drive/git_repos/archiso_profile $current_dir
 
-(cd $current_dir/archiso_profile; git stash pop)
+(
+    cd $current_dir/archiso_profile;
+    git pull --ff-only
+    git stash pop;
+)
 
-(cd $current_dir/archiso_profile/airootfs/root/archconfig; git stash pop)
+(
+    cd $current_dir/archiso_profile/airootfs/root/archconfig;
+    git pull --ff-only
+    git stash pop;
+)
 
 sudo mkarchiso -v archiso_profile
 
