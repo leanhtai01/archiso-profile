7b684ca3b615f4278c27c8c1e32475a6dcc80302
leanhtai01 <leanhtai01@gmail.com>
2021-11-07 23:51:19 +0700

    add script install.sh

---
+++ airootfs/root/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+current_dir=$(dirname $0)
+
+$current_dir/mount_encrypted_data_drive.sh
+cp -r mount_point/git_repos/archconfig .
+
+(
+    cd archconfig;
+    git stash pop;
+)
+
+$current_dir/archconfig/install_arch_linux.sh
