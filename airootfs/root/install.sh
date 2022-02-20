ff0a4676558e1050fe0c5be80ec004882b84fcb6
leanhtai01 <leanhtai01@gmail.com>
2022-02-20 13:11:03 +0700

    auto detect whether dual-boot with Windows

--- airootfs/root/install.sh
+++ airootfs/root/install.sh
@@ -3,6 +3,8 @@
 set -e
 
 current_dir=$(dirname $0)
+device="nvme0n1"
+is_dual_boot="y"
 
 $current_dir/connect_wifi_iwd.sh
 $current_dir/mount_encrypted_data_drive.sh
@@ -14,4 +16,23 @@ cp -r mount_point/git_repos/archconfig .
     git stash pop;
 )
 
-$current_dir/archconfig/install_arch_linux.sh
+# check whether Windows partitions (1-4) is exists
+for i in {1..4}
+do
+    partition="${device}p${i}"
+    if ! [[ -b "$partition" ]]
+    then
+        is_dual_boot="n"
+        break
+    fi
+done
+
+if [[ "$is_dual_boot" -eq "y" ]]
+then
+    # dual-boot with Windows
+    sed -i "/^size_of_ram=16/s/16/5/" $current_dir/archconfig/system_install/set_install_info.sh
+    sed -i "/^user_choice=2/s/2/5/" $current_dir/archconfig/system_install/set_install_info.sh
+else
+    # only Arch Linux
+    $current_dir/archconfig/install_arch_linux.sh
+fi
