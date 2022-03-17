d6bae59423320c242974c4938cef2eca4433bb6d
leanhtai01 <leanhtai01@gmail.com>
2022-03-17 15:25:02 +0700

    not pull changes from remote repository when making iso

--- tools/make_custom_archiso.sh
+++ tools/make_custom_archiso.sh
@@ -8,7 +8,7 @@ cp -r /run/media/leanhtai01/data_drive/git_repos/archiso_profile $current_dir
 
 (
     cd $current_dir/archiso_profile;
-    git pull --ff-only
+    # git pull --ff-only
     git stash pop;
 )
 
