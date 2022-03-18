421fe7b9d4ad06323e0c237465061bc79e24e95e
leanhtai01 <leanhtai01@gmail.com>
2022-03-18 08:57:24 +0700

    only ask for password if drive not already mounted

--- airootfs/root/copy_archconfig.sh
+++ airootfs/root/copy_archconfig.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-password=
-
-if [ -z "$password" ]
-then
-    read -e -p "Enter disk's password: " password
-fi
-
 if ! [ -d "mount_point" ]
 then
+    password=
+
+    if [ -z "$password" ]
+    then
+        read -e -p "Enter disk's password: " password
+    fi
+
     mkdir -p mount_point
     printf "$password" | cryptsetup open /dev/sda1 data_drive -
     mount /dev/mapper/data_drive mount_point
